050710d89205cc6488692579d3200d27d6bee172
Влад Книгин <vlaknig@gmail.com>
2024-08-11 22:51:48 +0300

    редактирование слоёв

--- PetFamily.Domain/Pet.cs
+++ PetFamily.Domain/Pet.cs
@@ -1,28 +1,65 @@
-﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿using CSharpFunctionalExtensions;
 
 namespace PetFamily.Domain
 {
     public class Pet
     {
-        public Guid Id { get; set; }
-        public string Nickname { get; set; } = default!;
-        public string TypeOfAnimals { get; set; } = default!;
-        public string Description { get; set; } = default!;
-        public string BreedOfPet { get; set; } = default!;
-        public string Color { get; set; } = default!;
-        public string HealthInformation { get; set; } = default!;
-        public string Address { get; set; } = default!;
-        public double Weight { get; set; } = default!;
-        public PhysicalQuantity PhysicalQuantityWeight { get; set; } = default!;
-        public double Height { get; set; } = default!;
-        public PhysicalQuantity PhysicalQuantityHeight { get; set; } = default!;
-        public string PhoneNumber { get; set; } = default!;
-        public bool IsCastrated { get; set; } = default!;
-        public DateTime DateOfBirth { get; set; } = default!;
-        public bool IsVaccinated { get; set; } = default!;
-        public AssistanceStatus AssistanceStatus { get; set; } = default!;
-        public DetailsForAssistance DetailsForAssistance { get; set; } = default!;
-        public DateTime DateOfCreation { get; set; } = default!;
+        //ef core
+        private Pet()
+        {
 
+        }
+
+        private Pet(string s)
+        {
+
+        }
+
+        public Guid Id { get; protected set; }
+
+        public string Nickname { get; protected set; } = default!;
+
+        public string TypeOfAnimals { get; protected set; } = default!;
+
+        public string Description { get; protected set; } = default!;
+
+        public string BreedOfPet { get; protected set; } = default!;
+
+        public string Color { get; protected set; } = default!;
+
+        public string HealthInformation { get; protected set; } = default!;
+
+        public string Address { get; protected set; } = default!;
+
+        public double Weight { get; protected set; } = default!;
+
+        public PhysicalQuantity PhysicalQuantityWeight { get; protected set; } = default!;
+
+        public double Height { get; protected set; } = default!;
+
+        public PhysicalQuantity PhysicalQuantityHeight { get; protected set; } = default!;
+
+        public string PhoneNumber { get; protected set; } = default!;
+
+        public bool IsCastrated { get; protected set; } = default!;
+
+        public DateTime DateOfBirth { get; protected set; } = default!;
+
+        public bool IsVaccinated { get; protected set; } = default!;
+
+        public AssistanceStatus AssistanceStatus { get; protected set; } = default!;
+
+        public DetailsForAssistance DetailsForAssistance { get; protected set; } = default!;
+
+        public DateTime DateOfCreation { get; protected set; } = default!;
+
+        public static Result<Pet> Create()
+        {
+            //validation
+
+            var pet = new Pet();
+
+            return Result.Success(pet);
+        }
     }
 }
