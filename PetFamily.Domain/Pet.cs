f1b5bbfdd96b1cc3a2ebd18aee78a202ea4761ff
Влад Книгин <vlaknig@gmail.com>
2024-08-12 01:34:54 +0300

    добавлина валидация для создания pet

--- PetFamily.Domain/Pet.cs
+++ PetFamily.Domain/Pet.cs
@@ -11,9 +11,27 @@ namespace PetFamily.Domain
 
         }
 
-        private Pet(string nickname, string typeOfAnimals, string? description, string BreedOfPet, string? Color, string? HealthInformation, )
+        private Pet(string nickname, string typeOfAnimals, string? description, string breedOfPet, string? color,
+                    string? healthInformation, Address address, PhysicalQuantity weight, PhysicalQuantity height,
+                    PhoneNumber? phoneNumber, bool isCastrated, DateTime? dateOfBirth, bool isVaccinated,
+                    AssistanceStatus assistanceStatus, DetailsForAssistance detailsForAssistance, DateTime dateOfCreation)
         {
-
+            Nickname = nickname;
+            TypeOfAnimals = typeOfAnimals;
+            Description = description;
+            BreedOfPet = breedOfPet;
+            Color = color;
+            HealthInformation = healthInformation;
+            Address = address;
+            Weight = weight;
+            Height = height;
+            PhoneNumber = phoneNumber;
+            IsCastrated = isCastrated;
+            DateOfBirth = dateOfBirth;
+            IsVaccinated = isVaccinated;
+            AssistanceStatus = assistanceStatus;
+            DetailsForAssistance = detailsForAssistance;
+            DateOfCreation = dateOfCreation;
         }
 
         public Guid Id { get; protected set; }
@@ -30,7 +48,7 @@ namespace PetFamily.Domain
 
         public string? HealthInformation { get; protected set; }
 
-        public string Address { get; protected set; } = default!;
+        public Address Address { get; protected set; } = default!;
 
         public PhysicalQuantity Weight { get; protected set; } = default!;
 
@@ -50,11 +68,24 @@ namespace PetFamily.Domain
 
         public DateTime DateOfCreation { get; protected set; }
 
-        public static Result<Pet> Create()
+        public static Result<Pet> Create(string nickname, string typeOfAnimals, string? description, string breedOfPet, string? color,
+                                         string? healthInformation, Address address, PhysicalQuantity weight, PhysicalQuantity height,
+                                         PhoneNumber? phoneNumber, bool isCastrated, DateTime? dateOfBirth, bool isVaccinated,
+                                         AssistanceStatus assistanceStatus, DetailsForAssistance detailsForAssistance)
         {
-            //validation
+            if (string.IsNullOrWhiteSpace(nickname))
+                Result.Failure<Pet>("nickname is null or white space");
 
-            var pet = new Pet();
+            if (string.IsNullOrWhiteSpace(typeOfAnimals))
+                Result.Failure<Pet>("typeOfAnimals is null or white space");
+
+            if (string.IsNullOrWhiteSpace(breedOfPet))
+                Result.Failure<Pet>("breedOfPet is null or white space");
+
+            var dateOfCreation = DateTime.Now;
+
+            var pet = new Pet(nickname, typeOfAnimals, description, breedOfPet, color, healthInformation, address, weight, height, phoneNumber, isCastrated,
+                              dateOfBirth, isVaccinated, assistanceStatus, detailsForAssistance, dateOfCreation);
 
             return Result.Success(pet);
         }
