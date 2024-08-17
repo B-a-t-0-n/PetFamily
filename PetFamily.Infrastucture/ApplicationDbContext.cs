2840956bd3048fc02c7c4e7a99dae7110caefa7a
Влад Книгин <vlaknig@gmail.com>
2024-08-17 03:26:11 +0300

    Добавлен EfCore

---
+++ PetFamily.Infrastucture/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetFamily.Domain;
+
+namespace PetFamily.Infrastucture
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Volunteer> Volunteers => Set<Volunteer>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
