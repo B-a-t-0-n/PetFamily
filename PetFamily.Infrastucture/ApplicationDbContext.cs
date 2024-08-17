6c08c674acdafde9dfd8378b91ec2145aff773a7
Влад Книгин <vlaknig@gmail.com>
2024-08-17 04:28:04 +0300

    добавлен docker и добавлено подключение ApplicationDbContex к БД

--- PetFamily.Infrastucture/ApplicationDbContext.cs
+++ PetFamily.Infrastucture/ApplicationDbContext.cs
@@ -1,15 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using PetFamily.Domain;
 
 namespace PetFamily.Infrastucture
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext(IConfiguration configuration) : DbContext
     {
+        private const string DATABASE = "Database";
+
         public DbSet<Volunteer> Volunteers => Set<Volunteer>();
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseNpgsql(configuration.GetConnectionString(DATABASE));
         }
     }
 }
