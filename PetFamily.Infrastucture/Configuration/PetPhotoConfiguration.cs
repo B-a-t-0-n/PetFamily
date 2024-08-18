f62576ec3950e819690acc07995e672959deceb0
Влад Книгин <vlaknig@gmail.com>
2024-08-18 03:05:14 +0300

    добавлена конфигурация для petPhoto

---
+++ PetFamily.Infrastucture/Configuration/PetPhotoConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using PetFamily.Domain.Entity;
+using PetFamily.Domain.Shared;
+
+namespace PetFamily.Infrastucture.Configuration
+{
+    public class PetPhotoConfiguration : IEntityTypeConfiguration<PetPhoto>
+    {
+        public void Configure(EntityTypeBuilder<PetPhoto> builder)
+        {
+            builder.ToTable("pet_photo");
+
+            builder.HasKey(i => i.Id);
+
+            builder.Property(p => p.IsMain)
+                    .IsRequired()
+                    .HasMaxLength(Constants.MAX_LOW_TEXT_LENGTH)
+                    .HasColumnName("is_main");
+
+            builder.Property(p => p.Path)
+                    .IsRequired()
+                    .HasMaxLength(Constants.MAX_LOW_TEXT_LENGTH)
+                    .HasColumnName("path");
+        }
+    }
+}
