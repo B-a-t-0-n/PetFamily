1f8098b131bc7117ff5aaf8247494be25d104784
Влад Книгин <vlaknig@gmail.com>
2024-08-27 16:05:45 +0300

    никоторые изменения

--- PetFamily.Domain/SpeciesMenegment/Entity/Species.cs
+++ PetFamily.Domain/SpeciesMenegment/Entity/Species.cs
@@ -17,6 +17,7 @@ namespace PetFamily.Domain.SpeciesMenegment.Entity
         }
 
         public Name Name { get; private set; } = default!;
+
         public IReadOnlyList<Breed> breeds => _breeds;
 
         public static Result<Species> Create(SpeciesId id, Name name)
